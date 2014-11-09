23c8a5d342c6b0b889d6886f0e8dbee7e01e530a
Su, Ralph <suliangfei@gmail.com>
2014-11-09 13:13:37 +0800

    loader test

--- src/main/java/spcache/Loader.java
+++ src/main/java/spcache/Loader.java
@@ -3,24 +3,30 @@
  */
 package spcache;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Optional;
+import com.google.common.base.Stopwatch;
+import com.google.common.io.Files;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
 /**
@@ -29,23 +35,63 @@ import com.mongodb.MongoClient;
  */
 public class Loader {
 
+	private static final Logger logger = LoggerFactory.getLogger(Loader.class);
+
 	public static void main(String[] args) throws Exception {
+		loadMongoTest();
+
+	}
+
+	private static void loadMongoTest() throws Exception {
+		MongoDB mongo = new MongoDB();
+		DBCollection coll = mongo.getCollection();
+
+		// load csv data
+		List<String> files = Files.readLines(new File(MongoDB.class
+				.getResource("/flight_top1000.csv").getPath()), Charset
+				.forName("utf8"));
+		String[] headers = files.get(0).split(",");
+		
+		final int totalSize = 70000000;
+		Stopwatch watch = Stopwatch.createStarted();
+		for (int j = 0; j < totalSize / 700; j++) {
+		    List<DBObject> dbos = new ArrayList<DBObject>(files.size() - 1);
+    		for (int i = 1; i < files.size(); i++) {
+    			Optional<DBObject> op = mongo.convertTo(files.get(i).split(","),
+    					headers);
+    			if (op.isPresent()) {
+    				dbos.add(op.get());
+    			}
+    		}
+    		coll.insert(dbos);
+    		
+    		if ( (j % 100) == 0) {
+    		    logger.info("inserted " + j * 771 + " lines");
+    		}
+		}
+		
+		watch.stop();
+		logger.info(" all inserted using time :" + watch.elapsed(TimeUnit.MILLISECONDS) + " ms");
+	}
+
+	@SuppressWarnings("unused")
+	private static void loadSqlTest() throws SQLException {
 		DatabaseConn sqlConn = new DatabaseConn();
 
 		Connection conn = sqlConn.getConnection();
 		PreparedStatement stat = conn
 				.prepareStatement("select TOP 10 * from "
-						+ MongoDB.FLT_SHARE_SPECIAL_POLICY_OW + " WITH(NOLOCK) "
-						+ " WHERE  PolicyStatus=0 AND (InventoryType = 1 OR (InventoryType = 2 AND TicketInventory > SaledTicketNum))"
-						);
+						+ MongoDB.FLT_SHARE_SPECIAL_POLICY_OW
+						+ " WITH(NOLOCK) "
+						+ " WHERE  PolicyStatus=0 AND (InventoryType = 1 OR (InventoryType = 2 AND TicketInventory > SaledTicketNum))");
 		ResultSet rs = stat.executeQuery();
 		while (rs.next()) {
-			System.out.println( rs.getInt("ProductID") );
-			System.out.println( rs.getString("Dcity") );
-			System.out.println( rs.getString("Acity") );
+			System.out.println(rs.getInt("ProductID"));
+			System.out.println(rs.getString("Dcity"));
+			System.out.println(rs.getString("Acity"));
 		}
-
 	}
+
 }
 
 class MongoDB {
@@ -63,29 +109,69 @@ class MongoDB {
 
 		try {
 			BasicDBObject keys = new BasicDBObject();
-			keys.put(colFieldMapping.get("ProductID"), 1);
 			keys.put(colFieldMapping.get("Dcity"), 1);
 			keys.put(colFieldMapping.get("Acity"), 1);
+			keys.put(colFieldMapping.get("ProductID"), 1);
 			coll.createIndex(keys);
 		} catch (Exception e) {
 			logger.error("create mongo index failed", e);
-			;
 		}
 	}
-	
-	public String getMongoField(String sqlCol) {
+
+	public DBCollection getCollection() {
+		return coll;
+	}
+
+	private String getMongoField(String sqlCol) {
 		return colFieldMapping.get(sqlCol);
 	}
-	
-	// 
+
+	public Optional<DBObject> convertTo(String[] row, String[] header) {
+		if (row.length != header.length) {
+			return Optional.absent();
+		}
+		BasicDBObject dbo = new BasicDBObject();
+		for (int i = 0; i < row.length; i++) {
+			String key = header[i];
+			String mongoField = getMongoField(key);
+			if (mongoField != null) {
+				// add if defined
+				if (row[i].toString().isEmpty()) {
+					dbo.put(mongoField, row[i]);
+				} else if (row[i].toString().toLowerCase().equals("null")) {
+					dbo.put(mongoField, null);
+				} else {
+					try {
+						dbo.put(mongoField, Integer.parseInt(row[i].toLowerCase()));
+					} catch (Exception e) {
+						dbo.put(mongoField, row[i]);
+					}
+				}
+			}
+		}
+		return Optional.of((DBObject) dbo);
+	}
+
+	// TEST IF all ok to mongo, we might add other ASCII available character
+	private static char[] AVAILABLE_FIELD_NAME = new char[] { 'a', 'b', 'c',
+			'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+			'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C',
+			'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+			'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2',
+			'3', '4', '5', '6', '7', '8', '9', '`', '~', '!', '@', '#', '%',
+			'^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}',
+			':', ';', '"', '\'', '/', '?', '>', ',', '<', '|', '\\' };
 	private static Map<String, String> colFieldMapping = new HashMap<String, String>();
 	static {
 		try {
-			List<String> files = Files.readAllLines(Paths.get(MongoDB.class.getResource("/fields.txt").getPath()), Charset.defaultCharset());
+			List<String> files = Files.readLines(new File(MongoDB.class
+					.getResource("/fields.txt").getPath()), Charset
+					.defaultCharset());
 			int i = 0;
 			for (String s : files) {
-				if (!s.isEmpty()) {
-					colFieldMapping.put(s, "f" + (i++));
+				if (!s.trim().isEmpty()) {
+					colFieldMapping.put(s.replace(',', ' ').trim(),
+							String.valueOf(AVAILABLE_FIELD_NAME[i++]));
 				}
 			}
 			logger.info(colFieldMapping.toString());
@@ -93,7 +179,7 @@ class MongoDB {
 			logger.info("failed", e);
 		}
 	}
-	
+
 }
 
 class DatabaseConn {
